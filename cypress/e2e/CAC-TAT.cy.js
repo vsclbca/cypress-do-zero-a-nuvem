e4ddb33a3639464983052fd9314c01b8d0b74217
Vinicius Silva <vinicius.cunha@lbca.com.br>
2025-04-05 16:11:46 -0300

    repeat a test execution 5 times

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -340,16 +340,18 @@ describe('Central de Atendimento ao Cliente TAT', () => {
       .and('have.attr', 'href', 'privacy.html')
   })
 
-  it('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
+  Cypress._.times(5, () => {
+  
+    it('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
 
-    cy.contains('a', 'Política de Privacidade')
-      .invoke('removeAttr', 'target')
-      .click()
+      cy.contains('a', 'Política de Privacidade')
+        .invoke('removeAttr', 'target')
+        .click()
 
-    cy.contains('h1', 'CAC TAT - Política de Privacidade')
-      .should('be.visible')
+      cy.contains('h1', 'CAC TAT - Política de Privacidade')
+        .should('be.visible')
+    })
   })
-
 
 
 })
